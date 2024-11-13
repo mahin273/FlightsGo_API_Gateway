de7012049b3e1e76bb2db052056aa14297c612dc
Mahin Khan <md.mahin.bd18@gmail.com>
2024-11-13 12:01:00 +0600

    Added encryption in signup

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,8 @@
 const {
   Model
 } = require('sequelize');
+const bcrypt = require('bcrypt');
+const { SALT_ROUNDS } = require('../config/server-config');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -33,5 +35,10 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'User',
   });
+
+  User.beforeCreate(function encrypt(user) {
+    const encryptedPassword = bcrypt.hashSync(user.password, +SALT_ROUNDS);
+    user.password = encryptedPassword;
+  })
   return User;
 };
